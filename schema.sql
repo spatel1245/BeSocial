68365e63f47637a599024429683a8e5f79ff7c64
benzarombez20 <bez20@pitt.edu>
2023-03-27 13:31:54 -0400

    Added Primary and Foreign Key Constraints

--- schema.sql
+++ schema.sql
@@ -2,10 +2,12 @@ CREATE TABLE profile
 (
     userID INTEGER NOT NULL,
     name VARCHAR(50),
-    email VARCHAR(50),
+    email VARCHAR(50) not null,
     password VARCHAR(50),
     date_of_birth DATE,
     lastLogin TIMESTAMP
+
+    CONSTRAINT PK_PROFILE PRIMARY KEY (planId)
 );
 
 CREATE TABLE friend
@@ -14,6 +16,8 @@ CREATE TABLE friend
     userID2 INTEGER NOT NULL,
     JDate DATE,
     requestText VARCHAR(200)
+
+    CONSTRAINT PK_friend PRIMARY KEY (userId1,userID2)
 );
 
 CREATE TABLE pendingFriend
@@ -21,6 +25,8 @@ CREATE TABLE pendingFriend
     userID1 INTEGER NOT NULL,
     userID2 INTEGER NOT NULL,
     requestText VARCHAR(200)
+
+CONSTRAINT PK_pendingFriend PRIMARY KEY (userId1,userID2)
 );
 
 CREATE TABLE groupInfo
@@ -29,6 +35,9 @@ CREATE TABLE groupInfo
     name VARCHAR(50),
     size INTEGER,
     description VARCHAR(200)
+
+    CONSTRAINT PK_groupInfo PRIMARY KEY (gID)
+
 );
 
 CREATE TABLE groupMember
@@ -37,6 +46,9 @@ CREATE TABLE groupMember
     userID1 INTEGER NOT NULL,
     role VARCHAR(20),
     lastConfirmed TIMESTAMP
+    CONSTRAINT PK_groupMember PRIMARY KEY (gID, UserID)
+    CONSTRAINT FK_GroupMember FOREIGN KEY userID1 REFERENCES PROFILE(UserID)
+    CONSTRAINT FK_GroupMember1 FOREIGN KEY gID REFERENCES GroupInfo(GID)
 );
 
 CREATE TABLE pendingGroupMember
@@ -45,6 +57,10 @@ CREATE TABLE pendingGroupMember
     userID INTEGER NOT NULL,
     requestText VARCHAR(200),
     requestTime TIMESTAMP
+
+CONSTRAINT PK_groupMember PRIMARY KEY (gID, UserID)
+CONSTRAINT FK_GroupMember FOREIGN KEY userID1 REFERENCES PROFILE(UserID)
+CONSTRAINT FK_GroupMember1 FOREIGN KEY gID REFERENCES GroupInfo(GID)
 );
 
 CREATE TABLE message
@@ -55,6 +71,10 @@ CREATE TABLE message
     toUserID INTEGER NOT NULL,
     toGroupID INTEGER,
     timeSent TIMESTAMP NOT NULL
+    CONSTRAINT PK_groupMember PRIMARY KEY (gID, UserID)
+    CONSTRAINT FK_GroupMember FOREIGN KEY userID1 REFERENCES PROFILE(UserID)
+    CONSTRAINT FK_GroupMember1 FOREIGN KEY gID REFERENCES GroupInfo(GID)
+
 );
 
 CREATE TABLE messageRecipient
