ad5e38aa73bcd2f33199555c56ffc56e6b404f58
benzarombez20 <71410819+benzarombez20@users.noreply.github.com>
2023-04-03 19:02:53 -0400

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,20 +1,29 @@
---1. addMessageRecipient which adds a corresponding entry into the messageRecipient relation upon adding a new message to the message relation
-
 CREATE OR REPLACE FUNCTION add_message_recipient()
+ RETURNS TRIGGER
+ AS $$
+ BEGIN
+     INSERT INTO messageRecipient (msgID, userID) VALUES (NEW.msgID, NEW.toUserID); --might need changed
+     RETURN NEW;
+ END;
+ $$ LANGUAGE plpgsql;
+
+ DROP TRIGGER if EXISTS add_message_recipient on message;
+ CREATE TRIGGER add_message_recipient
+ AFTER INSERT ON message
+ FOR EACH ROW
+ EXECUTE FUNCTION add_message_recipient();
+
+CREATE OR REPLACE FUNCTION delete_pending_friendRequest()
 RETURNS TRIGGER
-AS $$
-BEGIN
-    INSERT INTO messageRecipient (msgID, userID) VALUES (NEW.msgID, NEW.toUserID); --might need changed
+ AS $$
+ BEGIN
+    DELETE FROM pendingfriend WHERE pendingFriend.userID1= NEW.userID1;
     RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+ END;
+ $$ LANGUAGE plpgsql;
 
-DROP TRIGGER if EXISTS add_message_recipient on message;
-CREATE TRIGGER add_message_recipient
-AFTER INSERT ON message
-FOR EACH ROW
-EXECUTE FUNCTION add_message_recipient();
-
--- 2.updateGroup which moves a pending accepted request in the pendingGroupMember relation to the group Memberrelation when a member leaves the group.
-
-
+ DROP TRIGGER if EXISTS delete_pending_friendRequest on friend;
+ CREATE TRIGGER delete_pending_friendRequest
+ AFTER INSERT ON friend
+ FOR EACH ROW
+ EXECUTE FUNCTION delete_pending_friendRequest();
