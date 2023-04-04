1b4a1b72006140bdd9f58b064f90574eb82d2ef4
benzarombez20 <bez20@pitt.edu>
2023-04-04 10:49:48 -0400

    Added assumptions edited trigger

--- sample-data.sql
+++ sample-data.sql
@@ -1,5 +1,11 @@
 INSERT INTO profile values(3,'dog', 'benzarom@gmail.com','dog','2023-1-10', NOW());
 INSERT INTO profile values(1,'dog', 'benzarom@gmail.com','dog','2023-1-10', NOW());
+INSERT INTO profile values(2,'dog', 'benzarom@gmail.com','dog','2023-1-10', NOW());
+INSERT INTO profile values(4,'dog', 'benzarom@gmail.com','dog','2023-1-10', NOW());
+
+
 INSERT INTO message values (1, 3, 'hi',3,4,NOW());
 INSERT INTO pendingfriend values(1,3,'Be my friend');
 INSERT INTO friend values(1,3,'2023-1-10','yes')
+INSERT INTO pendingfriend values(4,2,'Be my friend');
+INSERT INTO friend values(2,4,'2023-1-10','Be my friend');
--- schema.sql
+++ schema.sql
@@ -15,21 +15,22 @@ DROP TABLE IF EXISTS clock CASCADE;
 
 CREATE TABLE profile
 (
-    userID INTEGER NOT NULL,
-    name VARCHAR(50),
-    email VARCHAR(50) not null,
-    password VARCHAR(50),
-    date_of_birth DATE not null,
-    lastLogin TIMESTAMP not null,
+    userID INTEGER NOT NULL,--Assume user ID is needed to identify user.
+    name VARCHAR(50) not null,
+    email VARCHAR(50) not null, --Assume emails must be unique since email is required to make an account.
+    password VARCHAR(50) not null, --Password cannot be empty
+    date_of_birth DATE not null, --Users cannot be born before the year 2010.
+    lastLogin TIMESTAMP not null, --
 
-    CONSTRAINT PK_PROFILE PRIMARY KEY (userID) -- userID is the primary key for the profile table as it is unique for each user and cannot be null as each profile must have an ID
+    CONSTRAINT PK_PROFILE PRIMARY KEY (userID), -- userID is the primary key for the profile table as it is unique for each user and cannot be null as each profile must have an ID
+    CONSTRAINT UQ_EMAIL UNIQUE (email)
 );
 
 CREATE TABLE friend
 (
     userID1 INTEGER NOT NULL,
     userID2 INTEGER NOT NULL,
-    JDate DATE,
+    JDate DATE NOT NULL       DEFAULT CURRENT_DATE,-- Assume that date cannot be before the application was released. January 1st 2015.
     requestText VARCHAR(200),
 
     CONSTRAINT PK_FRIEND PRIMARY KEY(userID1, userID2) --Both userID1 and userID2 are the primary key because to uniquely identify a friendship, both friends (userIDs) are necessary
@@ -37,7 +38,7 @@ CREATE TABLE friend
 
 CREATE TABLE pendingFriend
 (
-    userID1 INTEGER NOT NULL,
+    userID1 INTEGER NOT NULL, --Assume that If friend tuple exists between user1 and 2 pendingFriend between users cannot exist.
     userID2 INTEGER NOT NULL,
     requestText VARCHAR(200),
 
@@ -46,10 +47,10 @@ CONSTRAINT PK_pendingFriend PRIMARY KEY (userId1, userID2) --similar to the frie
 
 CREATE TABLE groupInfo
 (
-    gID INTEGER NOT NULL,
-    name VARCHAR(50),
-    size INTEGER,
-    description VARCHAR(200),
+    gID INTEGER NOT NULL, --Assume that groupId is necessary to identify group as a primary key.
+    name VARCHAR(50) NOT NULL, --Assume that group name cannot be empty.
+    size INTEGER NOT NULL, --Assume that the group size must be positive and less than 32 users.
+    description VARCHAR(200) NOT NULL,
 
     CONSTRAINT PK_groupInfo PRIMARY KEY (gID) -- gID (GroupID) is the primary key as each group has its own unique ID, and this can never be null
 
@@ -57,22 +58,23 @@ CREATE TABLE groupInfo
 
 CREATE TABLE groupMember
 (
-    gID INTEGER NOT NULL,
-    userID1 INTEGER NOT NULL,
-    role VARCHAR(20),
-    lastConfirmed TIMESTAMP,
-    CONSTRAINT PK_groupMember PRIMARY KEY (gID, userID1),-- gID and userID(1) are the primary key for this relation as a groupMember is a user(profile) that belongs to a group. Both are necessary to uniquely identify the group member.
-    CONSTRAINT FK_GroupMember FOREIGN KEY (userID1) REFERENCES profile(UserID), -- The UserID in this relation belongs to a user of the social media profile. This reference establishes a connection between their attributes and belonging to the group, as it allows for access to the user's attributes.
-    CONSTRAINT FK_GroupMember1 FOREIGN KEY (gID) REFERENCES GroupInfo(GID) -- The gID in this relation is a group in this social media platform. This reference allows for a connection between the member and the group's attributes that they belong to, as it allows for access to the group's attributes.
+    gID INTEGER NOT NULL,--Assume that groupId is necessary to identify group as a primary key.
+    userID INTEGER NOT NULL,--Assume that userID cannot be null to identify which profile is in the group.
+    role VARCHAR(20) NOT NULL       DEFAULT 'member', --Assume that each member must be be a manager or a member to enable appropriate permissions
+    lastConfirmed TIMESTAMP NOT NULL DEFAULT current_time,--Assume that this is the time that the user was accepted into the group.
+
+    CONSTRAINT PK_groupMember PRIMARY KEY (gID, userID),-- gID and userID(1) are the primary key for this relation as a groupMember is a user(profile) that belongs to a group. Both are necessary to uniquely identify the group member.
+    CONSTRAINT FK_GroupMember FOREIGN KEY (userID) REFERENCES profile(UserID), -- The UserID in this relation belongs to a user of the social media profile. This reference establishes a connection between their attributes and belonging to the group, as it allows for access to the user's attributes.
+    CONSTRAINT FK_GroupMember1 FOREIGN KEY (gID) REFERENCES GroupInfo(gID)  -- The gID in this relation is a group in this social media platform. This reference allows for a connection between the member and the group's attributes that they belong to, as it allows for access to the group's attributes.
 
 );
 
 CREATE TABLE pendingGroupMember
 (
-    gID INTEGER NOT NULL,
-    userID INTEGER NOT NULL,
-    requestText VARCHAR(200),
-    requestTime TIMESTAMP,
+    gID INTEGER NOT NULL,--Assume that gID is cannot be null to identify which group the profile is attempting to join.
+    userID INTEGER NOT NULL,--Assume that userID cannot be null to identify which profile is attempting to join the group.
+    requestText VARCHAR(200) NOT NULL DEFAULT 'I would like to join your group!',--
+    requestTime TIMESTAMP NOT NULL DEFAULT current_time,-- Assume that request time cannot be null 
 
 CONSTRAINT PK_pendingGroupMember PRIMARY KEY (gID, UserID), --similar to the groupMember relation, in order to uniquely identify a member's pending request to join a group, both the user that is requesting to join, and the group's ID are necessary.
 CONSTRAINT FK_pendingGroupMember FOREIGN KEY (userID) REFERENCES profile(UserID), -- The UserID in this relation belongs to a user of the social media profile. This reference establishes a connection between the user's attributes and request to join the group, as it allows for access to the user's attributes.
@@ -107,3 +109,9 @@ CREATE TABLE clock
     pseudo_time TIMESTAMP,
     CONSTRAINT PK_Clock PRIMARY KEY (pseudo_time)
 );
+
+
+
+
+
+
