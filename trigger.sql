01b1cef8b50bf384ebeeb683e62df61f2255ac5c
Shiv Patel <shivpatel1245@gmail.com>
2023-04-18 22:35:22 -0400

    Remove unnecessary files and combine functions/proc. into trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -371,3 +371,25 @@ $$;
 -----------------------------------------------------------------
 --END PROCEDURE 6 confirmGroupMembers
 -----------------------------------------------------------------
+
+DROP VIEW IF EXISTS group_reqs_to_accept;
+
+-- CREATE VIEW group_reqs_to_accept AS
+-- SELECT p.*
+-- FROM pendingGroupMember p
+--          JOIN groupMember g ON p.gID = g.gID
+-- WHERE p.userID = 0 AND g.role = 'manager';
+
+DROP FUNCTION IF EXISTS get_pending_members(user_id INTEGER);
+
+CREATE OR REPLACE FUNCTION get_pending_members(p_user_id INTEGER)
+    RETURNS SETOF pendingGroupMember AS $$
+BEGIN
+    EXECUTE 'CREATE OR REPLACE VIEW group_reqs_to_accept AS
+             SELECT p.*
+             FROM pendingGroupMember p
+             JOIN groupMember g ON p.gID = g.gID
+             WHERE g.userID = ' || p_user_id || ' AND g.role = ''manager'';';
+    RETURN QUERY SELECT * FROM group_reqs_to_accept;
+END;
+$$ LANGUAGE plpgsql;
